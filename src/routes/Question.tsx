ef20a1ad4b75949b8a63e5b599292419387a38c4
Masaru Hori <qirex_rr@yahoo.co.jp>
2022-09-07 22:17:32 +0900

    SPACE で次に

--- src/routes/Question.tsx
+++ src/routes/Question.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Input from "@mui/material/Input";
 import InputAdorment from "@mui/material/InputAdornment";
 import Check from "@mui/icons-material/Check";
@@ -7,48 +7,98 @@ import Call from "@mui/icons-material/Call";
 import Button from "@mui/material/Button";
 import { QuestionData, divideQuestion } from "../questions";
 
+const finishButtonId = "finishButton";
+
+const getQuestionId = (questionNo: number, answerNo: number): string => {
+  return `question-${questionNo}-${answerNo}`;
+};
+
 const QuestionLine = ({
   question,
+  questionNo,
   correctOrWrong,
   setCorrectWrong,
 }: {
   question: QuestionData;
+  questionNo: number;
   correctOrWrong: number;
   setCorrectWrong: (value: number) => void;
 }) => {
+  const [nextElement, setNextElement] = React.useState<HTMLElement | null>(
+    null
+  );
   const NONE: number = 0;
   const CORRECT: number = 1;
   const WRONG: number = -1;
   const HINT: number = -2;
   const isHintUsed: boolean = correctOrWrong === HINT;
+  const answerKazu = question.answerPosition.length;
+
+  useEffect(() => {
+    nextElement?.focus();
+    setNextElement(null);
+  }, [nextElement]);
 
   const handleBlur = (
     e: React.FocusEvent<HTMLInputElement>,
-    answer_moji: string
+    answerMoji: string
   ): void => {
     if (e.target.value.length === 0) return;
 
     if (isHintUsed) {
-      e.target.value = answer_moji;
-    } else if (e.target.value.toLowerCase() === answer_moji.toLowerCase()) {
-      e.target.value = answer_moji;
+      e.target.value = answerMoji;
+    } else if (
+      e.target.value.trim().toLowerCase() === answerMoji.toLowerCase()
+    ) {
+      e.target.value = answerMoji;
       setCorrectWrong(CORRECT);
     } else {
       setCorrectWrong(WRONG);
     }
   };
-  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    if (isHintUsed) return;
+  const handleFocus = (
+    e: React.FocusEvent<HTMLInputElement>,
+    answerMoji: string
+  ) => {
+    if (isHintUsed) {
+      e.target.value = answerMoji;
+      return;
+    }
 
     setCorrectWrong(NONE);
   };
   const handleChange = (
     e: React.FocusEvent<HTMLInputElement>,
-    answer_moji: string
+    answerMoji: string,
+    answerNo: number
   ): void => {
-    if (e.target.value === " ") {
-      e.target.value = answer_moji;
+    const isLastAnswer: boolean = answerKazu <= answerNo + 1;
+    const nextAnswerNo = isLastAnswer ? 0 : answerNo + 1;
+    const nextQuestionNo = isLastAnswer ? questionNo + 1 : questionNo;
+    const nextElementId = getQuestionId(nextQuestionNo, nextAnswerNo);
+    const nextElement: HTMLElement | null =
+      document.getElementById(nextElementId) ||
+      document.getElementById(finishButtonId);
+
+    if (e.target.value.search(/\?/) === 0) {
+      e.target.value = answerMoji;
       setCorrectWrong(HINT);
+      setNextElement(nextElement);
+    } else if (e.target.value.search(/ $/) >= 0) {
+      if (isHintUsed) {
+        e.target.value = answerMoji;
+      } else if (e.target.value.search(/ /) === 0) {
+        e.target.value = "";
+      } else if (
+        e.target.value.trim().toLowerCase() === answerMoji.toLowerCase()
+      ) {
+        e.target.value = answerMoji;
+        setCorrectWrong(CORRECT);
+      } else {
+        e.target.value = e.target.value.trim();
+        setCorrectWrong(WRONG);
+      }
+      setNextElement(nextElement);
     }
   };
 
@@ -80,19 +130,23 @@ const QuestionLine = ({
     } else {
       const answer = partOfEnglish;
       const separater: string = answer.match(/[?.,:;]$/)?.[0] || "";
-      const answer_moji: string = answer.match(/[a-zA-Z\-']+/)?.[0] || "";
+      const answerMoji: string = answer.match(/[a-zA-Z\-']+/)?.[0] || "";
+      const answerNo: number = Math.floor(i / 2);
 
       return (
         <span key={i}>
           <Input
             type="text"
             color="secondary"
+            id={getQuestionId(questionNo, answerNo)}
             onBlur={(e: React.FocusEvent<HTMLInputElement>) =>
-              handleBlur(e, answer_moji)
+              handleBlur(e, answerMoji)
             }
-            onFocus={handleFocus}
+            onFocus={(e: React.FocusEvent<HTMLInputElement>) =>
+              handleFocus(e, answerMoji)
+            }
             onChange={(e: React.FocusEvent<HTMLInputElement>) =>
-              handleChange(e, answer_moji)
+              handleChange(e, answerMoji, answerNo)
             }
             style={{ width: answer.length * 16 + 24, fontSize: "100%" }}
             autoComplete="off"
@@ -127,6 +181,7 @@ const Questions = ({
           <QuestionLine
             key={i}
             question={v}
+            questionNo={i}
             correctOrWrong={questions[i].correctOrWrong}
             setCorrectWrong={(value: number) => updateCorrectWrong(i, value)}
           />
@@ -145,6 +200,10 @@ export const Question = ({
   updateCorrectWrong: (idx: number, status: number) => void;
   finished: () => void;
 }) => {
+  useEffect(() => {
+    document.getElementById(getQuestionId(0, 0))?.focus();
+  }, []);
+
   return (
     <>
       <Questions
@@ -152,7 +211,9 @@ export const Question = ({
         updateCorrectWrong={updateCorrectWrong}
       />
       <hr></hr>
-      <Button onClick={finished}>FINISHED</Button>
+      <Button id={finishButtonId} onClick={finished}>
+        FINISHED
+      </Button>
       {questions.reduce((prev, question) => {
         if (question.correctOrWrong > 0) prev += 1;
         return prev;
